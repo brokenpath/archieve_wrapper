ee2ec268e53e07821b87468e7cf1112411487ccf
brokenpath <brokenpath@nowhere.com>
2021-01-07 16:33:24 +0100

    lower the bar to see how things play out before committing

--- archivewrapper/src/main/scala/archive/Archiver.scala
+++ archivewrapper/src/main/scala/archive/Archiver.scala
@@ -1,5 +1,32 @@
 package archive
 
+import org.apache.commons.compress.archivers.ArchiveEntry
+import java.io.InputStream
+import org.apache.commons.compress.archivers.ArchiveOutputStream
+import scala.util.Try
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry
+import java.nio.file.attribute.FileTime
+import org.apache.commons.compress.utils.IOUtils
+
 object Archiver {
+    case class ArFile(entry: ArchiveEntry, input: InputStream)
+    case class Leaf(name: String)
+    case class OutPutArchive()
+    case class ArMapping(inputFileName: String, outputFileName, rootArchive: String)
+
+    def repack(file: ArFile, l: Leaf, os: ArchiveOutputStream) : Try[Unit] = Try {
+        val entry = new ZipArchiveEntry(l.name)
+        val time = FileTime.from(entry.getLastModifiedDate().toInstant())
+        entry.setLastModifiedTime(time)
+        entry.setSize(file.entry.getSize())
+        os.putArchiveEntry(entry)
+        IOUtils.copy(file.input, os)
+        os.closeArchiveEntry()
+    }
+
+    
+    def copy(input: String, mapping : Seq[ArMapping] ) = ???
+
+
   
 }
--- archivewrapper/src/main/scala/archive/CompressionName.scala
+++ archivewrapper/src/main/scala/archive/CompressionName.scala
@@ -1,13 +1,15 @@
 package archive
 
 import scala.collection.immutable.HashMap
+import java.io.File
 
 sealed trait Compression
 
 case class Undetectable() extends Compression
 case class Name( name :String) extends Compression
 
-
+// ArchiveEntry
+// ArchiveOutputStream
 
 
 //Input
@@ -15,9 +17,14 @@ sealed trait Structure
 case class ArchiveStructure(name: String, value:  HashMap[ArchiveFile,Structure]) extends Structure
 case class ArchiveFile(name:String) extends Structure
 
-//Output
+//tree reversed pointer Output, the mapping are from archive to archive/file and root is always a File
 sealed trait Tree
-case class ArchiveNode[A](name: String, parent : Tree, output: Option[A]) extends Tree
-case class Root[A](filename: String, output: Option[A]) extends Tree
+case class ArchiveNode[A](name: String, parent : Tree, leafs: HashMap[Leaf, A], output: Option[A]) extends Tree
+case class Leaf(name: String)
+case class Root(filename: String, output: Option[File]) extends Tree
 
 case class Path( path: Seq[Tree])
+
+
+
+//  (ArchiveEntry, InputStream)
